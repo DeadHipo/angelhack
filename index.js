c6f3b1ebb66b2467641d1df398f34a10b55c8813
Alexsander <561298@gmail.com>
2016-06-11 16:54:43 +0300

    log connect db

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ var user = require('./Controller/UserController');
 
 mongoose.connect('mongodb://localhost/db');
 
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('connection error:', err.message);
+});
+db.once('open', function callback () {
+    console.log("Connected to DB!");
+});
+
+
 app.use('/user', user);
 
 app.get('/', function (req, res) {
