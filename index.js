f084c01f8d42f013df5c12d616f8c117fe07f148
Alexsander <561298@gmail.com>
2016-06-11 16:36:56 +0300

    registation router

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+var user = require('./Controller/UserController');
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/db');
+
+app.use('/user', user);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
